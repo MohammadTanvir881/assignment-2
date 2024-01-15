42e2392b083cc0acfdc5822d743894a84225896e
MohammadTanvir881 <tanvirrashid881@gmail.com>
2024-01-15 11:20:19 -0500

    popular section build and responsive done

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-git
+const navTogger = document.querySelector(".nav-toggle");
+navTogger.addEventListener("click", navToggo);
+
+function navToggo(){
+    //  navTogger.classList.toggle("active");
+     const nav = document.querySelector("nav");
+     nav.classList.toggle("open");
+     if(nav.classList.contains("open") ){
+         nav.style.maxHeight = nav.scrollHeight + "px";
+     }else{
+         nav.removeAttribute("style")
+     }
+    ;
+}
